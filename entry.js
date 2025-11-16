1d4bb19a4f7fd04d5e858f9423b26ff556ef1b08
NTiriu <37409548+NTiriu@users.noreply.github.com>
2025-11-16 11:57:30 +0000

    zoomin

--- entry.js
+++ entry.js
@@ -1,1 +1,10 @@
-alert("Good morining to the World")
+// alert("Good morining to the World")
+import * as maplibregl from "https://cdn.skypack.dev/maplibre-gl";
+
+const map = new maplibregl.Map({
+    container: 'map', // container id
+    // style: 'https://demotiles.maplibre.org/style.json', // style URL
+    style: 'https://tiles.openfreemap.org/styles/liberty', 
+    center: [0.1276,51.5072], // starting position [lng, lat]  51.5072° N, 0.1276° W
+    zoom: 10 // starting zoom
+});
